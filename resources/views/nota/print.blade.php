4a45c1328d9973a727ad895f5289c231449d0bc3
Fihri Aziz <fihriaziz@gmail.com>
2023-11-11 09:38:24 +0700

    chore: design nota pembelian

--- resources/views/nota/print.blade.php
+++ resources/views/nota/print.blade.php
@@ -10,9 +10,14 @@
                 border : 2px solid #333;
                 text-align: center;
             }
+            div {
+                display: flex;
+                justify-content: center
+            }
 
             table {
                 border-collapse: collapse;
+                width: 43%
             }
 
             th, td {
@@ -22,31 +27,35 @@
 
  </head>
  <body>
-    <table class="table table-striped table-bordered">
-        <thead>
-            <tr>
-                <th colspan="4"><h3>Invoice : {{ $notas->invoice }} </h3></th>
-            </tr>
-            <tr>
-                <th>Product</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>SubTotal</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($notas->details as $nota)
-            <tr>
-                <td>{{ $nota->product->name }}</td>
-                <td>{{ $nota->qty }}</td>
-                <td>{{ number_format($nota->product->price) }}</td>
-                <td>{{ number_format($nota->product->price * $nota->qty) }}</td>
-            </tr>
-            @endforeach
-            <td colspan="3" class="text-center">Total</td>
-                <td>{{ number_format($notas->subtotal) }}</td>
-        </tbody>
-    </table>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">
+                        <h3>Invoice : {{ $notas->invoice }} </h3>
+                    </th>
+                </tr>
+                <tr>
+                    <th>Product</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>SubTotal</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($notas->details as $nota)
+                <tr>
+                    <td>{{ $nota->product->name }}</td>
+                    <td>{{ $nota->qty }}</td>
+                    <td>Rp. {{ number_format($nota->product->price) }}</td>
+                    <td>Rp. {{ number_format($nota->product->price * $nota->qty) }}</td>
+                </tr>
+                @endforeach
+                <td colspan="3" class="text-center">Total</td>
+                    <td>Rp. {{ number_format($notas->subtotal) }}</td>
+            </tbody>
+        </table>
+    </div>
  </body>
  </html>
 <script>
