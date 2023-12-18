c189ed7b248ecfdfa5e66e193bc4d1a1c9f75387
Fihri Aziz <fihriaziz@gmail.com>
2023-12-18 13:02:08 +0700

    fix: refactor cetak pdf

--- resources/views/cetak.blade.php
+++ resources/views/cetak.blade.php
@@ -15,6 +15,10 @@
                border-collapse: collapse;
                width: 100%;
            }
+           .text-danger {
+            color: rgb(221, 47, 47);
+            font-weight: bold
+           }
 
            th, td {
                padding: 3px;
@@ -34,18 +38,22 @@
            </tr>
        </thead>
        <tbody>
-           @foreach ($notas as $nota)
+           @forelse ($notas as $nota)
            <tr>
                <td>{{ $nota->invoice }}</td>
                @foreach ($nota->details as $item)
-                <td>{{ $item->product->name }}</td>
-                <td>{{ number_format($item->qty) }}</td>
-                <td>{{ number_format($item->product->price, 0, ",",".") }}</td>
-                <td>{{ number_format($nota->subtotal, 0, ",",".") }}</td>
+                    <td>{{ $item->product->name }}</td>
+                    <td>{{ number_format($item->qty) }}</td>
+                    <td>{{ number_format($item->product->price, 0, ",",".") }}</td>
+                    <td>{{ number_format($nota->subtotal, 0, ",",".") }}</td>
                @endforeach
            </tr>
-           @endforeach
-       </tbody>
+           @empty
+           <tr>
+                <td colspan="5"><span class="text-danger">{{'Data Not Found'}}</span></td>
+            </tr>
+           @endforelse
+        </tbody>
    </table>
 </body>
 </html>
